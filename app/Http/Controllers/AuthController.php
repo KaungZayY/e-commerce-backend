d7a3905aba64830e8c7adb89c42d22bd0dbff4e5
KaungZayY <kaungzayyan19@gmail.com>
2025-10-29 22:57:25 +0700

    User Register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,11 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    public function register(Request $req)
+    {
+        $req->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|string|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $req['name'],
+            'email' => $req['email'],
+            'user_role_id' => 2, // customer
+            'password' => Hash::make($req['password'])
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+        return response([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
     public function login(Request $req)
     {
         $cred = $req->validate([
